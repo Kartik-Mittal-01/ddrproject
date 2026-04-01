3ebc15d1c7c1eab1a6f481a0fc6919c4d5f79a67
Kartik Mittal <kartikmittal8998@gmail.com>
2026-04-01 21:17:09 +0530

    Initial Commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import Card from 'react-bootstrap/Card';
+
+function TextExample({ title, subtitle, description }) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          {subtitle}
+        </Card.Subtitle>
+        <Card.Text>
+          {description}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default TextExample;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Card from '../components/Card'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Card 
+            title="Hello" 
+            subtitle="kartik" 
+            description="hello this is kartik" 
+        />
+        <Card 
+            title="Hello" 
+            subtitle="kartik" 
+            description="hello this is kartik" 
+        />
+        <Card 
+            title="Hello" 
+            subtitle="kartik" 
+            description="hello this is kartik" 
+        />
+        <Card 
+            title="Hello" 
+            subtitle="kartik" 
+            description="hello this is kartik" 
+        />
+        <Card 
+            title="Hello" 
+            subtitle="kartik" 
+            description="hello this is kartik" 
+        />
+        
+    </div>
+  )
+}
+
+export default HomePage
